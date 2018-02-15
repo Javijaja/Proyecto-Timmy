8a132dd4797404f46c820f4c8dd376058f9903e2
Javijaja <36373276+Javijaja@users.noreply.github.com>
2018-02-15 17:28:44 +0100

    Update p1e2.vhd

--- p1e2.vhd
+++ p1e2.vhd
@@ -31,7 +31,6 @@ architecture Deco3a8reg of Deco3a8Reg is
     );
   end component;
   begin
-    --Soy un poco disminuido y he puesto los mismos nombres lo se im a fucking genius.
   u1:Deco3a8 port map(D(0)=>D(0),D(1)=>D(1),D(2)=>D(2),S(0)=>S(0),S(1)=>S(1),S(2)=>S(2),S(3)=>S(3),S(4)=>S(4),S(5)=>S(5),S(6)=>S(6),S(7)=>S(7));
   u2:Registro port map(D(0)=>S(0),D(1)=>S(1),D(2)=>S(2),D(3)=>S(3),D(4)=>S(4),D(5)=>S(5),D(6)=>S(6),D(7)=>S(7),Q(0)=>Q(0),Q(1)=>Q(1),Q(2)=>Q(2),Q(3)=>Q(3),Q(4)=>Q(4),Q(5)=>Q(5),Q(6)=>Q(6),Q(7)=>Q(7),CE=>CE,Clk=>Clk,Reset=>Reset);
 end Deco3a8reg;
